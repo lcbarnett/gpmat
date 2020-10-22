b96240206dc7e72abca04372c1ea93a2a2ff6bac
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2020-10-22 18:14:45 +0100

    Default positions for numeric tics

--- gp/gp_numtics.m
+++ gp/gp_numtics.m
@@ -1,9 +1,13 @@
 function tics = gp_numtics(num,pos)
 
 assert(isvector(num),'numbers must be a vector');
-assert(isvector(pos),'positions must be a vector');
 n = length(num);
-assert(length(pos) == n,'numbers must match positions');
+if nargin < 2 || isempty(pos)
+	pos = num;
+else
+	assert(isvector(pos),'positions must be a vector');
+	assert(length(pos) == n,'numbers must match positions');
+end
 
 tics = ['"' num2str(num(1)) '" ' num2str(pos(1))];
 for i = 2:n
